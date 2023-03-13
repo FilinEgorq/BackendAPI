df75c7b729031cff1e4cce5f04d1bf5773dde009
FilinEgorq <filin.egor204@gmail.com>
2023-03-13 11:24:25 +0300

    Продолжение разработки API (Создание сервисов)

---
+++ BusinessLogic/Services/GoodCharachteristicService.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Interfaces;
+using Domain.Models;
+
+namespace BusinessLogic.Services
+{
+    public class GoodCharachteristicService
+    {
+        private IRepositoryWrapper _repositoryWrapper;
+
+        public GoodCharachteristicService(IRepositoryWrapper repositoryWrapper) => _repositoryWrapper = repositoryWrapper;
+
+        public async Task<List<GoodCharachteristic>> GetAll() => await _repositoryWrapper.GoodCharachteristics.FindAll();
+
+        public async Task<GoodCharachteristic> GetById(int goodId, int filterName)
+        {
+            var user = await _repositoryWrapper.GoodCharachteristics.FindByCondition(x => x.GoodId == goodId && x.FilterName == filterName);
+
+            return user.First();
+        }
+
+        public async Task Create(GoodCharachteristic model)
+        {
+            await _repositoryWrapper.GoodCharachteristics.Create(model);
+            await _repositoryWrapper.Save();
+        }
+
+        public async Task Update(GoodCharachteristic model)
+        {
+            await _repositoryWrapper.GoodCharachteristics.Update(model);
+            await _repositoryWrapper.Save();
+        }
+
+        public async Task Delete(int goodId, int filterName)
+        {
+            var user = await _repositoryWrapper.GoodCharachteristics.FindByCondition(x => x.GoodId == goodId && x.FilterName == filterName);
+
+            await _repositoryWrapper.GoodCharachteristics.Delete(user.First());
+            await _repositoryWrapper.Save();
+        }
+    }
+}
--- Domain/Interfaces/IRepositoryWrapper.cs
+++ Domain/Interfaces/IRepositoryWrapper.cs
@@ -4,6 +4,18 @@
     {
         IUserRepository User { get; }
 
+        ICartRepository Cart { get; }
+
+        ICategoryRepository Category { get; }
+
+        IFilterRepository Filter { get; }
+
+        IGoodRepository Good { get; }
+
+        IGoodCharachteristicsRepository GoodCharachteristics { get; }
+
+        IOrderRepository Order { get; }
+
         Task Save();
     }
 }
