8a0ec840f1849e4ed6e99374837eac2fb7ef882b
FilinEgorq <filin.egor204@gmail.com>
2023-05-19 09:11:40 +0300

    Добавление пространства имён и сопоставления реализаций слоёв в Blazor-проект

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -1,10 +1,28 @@
+using BusinessLogic.Services;
+using DataAccess.Context;
+using DataAccess.Wrapper;
+using Domain.Interfaces;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using WebServer.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<InternetShopContext>(options =>
+								options.UseSqlServer("Server= DESKTOP-TKK652L;Database= InternetShop;Integrated Security= True;"));
+
+builder.Services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IFilterService, FilterService>();
+builder.Services.AddScoped<IGoodCharachteristicService, GoodCharachteristicService>();
+builder.Services.AddScoped<IGoodService, GoodService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
